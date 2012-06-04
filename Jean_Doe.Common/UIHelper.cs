56a36a44599e710ffa624cb8fa030c9013c5fe01
Fan Yang <vindurriel@gmail.com>
2012-06-04 16:01:54 +0800

    asd

---
+++ Jean_Doe.Common/UIHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Jean_Doe.Common
+{
+    public static class UIHelper
+    {
+        public static void RunOnUI(Action a)
+        {
+            System.Windows.Application.Current.Dispatcher.BeginInvoke(a);
+        }
+        public static void WaitOnUI(Action a)
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke(a);
+        }
+    }
+}
