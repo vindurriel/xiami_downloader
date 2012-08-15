e6ae7e5f16711bd2f9b6f214f549bd01015a3198
unknown <fan@vindurriel-nb.(none)>
2012-08-15 20:49:27 +0800

    Revert "fixed a http timeout bug"
    This reverts commit 0654c5a94834986f95a7c066f4d8a93fb2f82a73.

--- Jean_Doe.Common/NetAccess.cs
+++ Jean_Doe.Common/NetAccess.cs
@@ -46,7 +46,7 @@ namespace Jean_Doe.Common
                 var x = await client.GetAsync(url, tcs.Token);
                 res = await x.Content.ReadAsStringAsync();
             }
-            catch (Exception e)
+            catch (OperationCanceledException e)
             {
 
             }
