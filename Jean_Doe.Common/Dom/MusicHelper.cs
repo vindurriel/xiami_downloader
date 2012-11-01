4013f2b48340e1398e9608673f0d1ba6bc0c3e5f
Fan Yang <vindurriel@gmail.com>
2012-11-01 19:37:44 +0800

    changed the way to parse json to get song

--- Jean_Doe.Common/Dom/MusicHelper.cs
+++ Jean_Doe.Common/Dom/MusicHelper.cs
@@ -2,35 +2,39 @@
 using Jean_Doe.Common;
 internal static class MusicHelper
 {
-    public static void LoadMusicInfoFromJson(IMusic m, dynamic obj)
-    {
-        var type = m.Type.ToString();
-        m.Name = Get(obj, type+"_name","name");
-		m.Id = Get(obj, type + "_id", "id");
-        m.Logo =StringHelper.EscapeUrl(Get(obj,type + "_logo","logo"));
-    }
-    public static string Get(dynamic obj, string prop,string fallback=null)
+	public static void LoadMusicInfoFromJson(IMusic m, dynamic obj)
+	{
+		var type = m.Type.ToString();
+		m.Name = Get(obj,  type + "_name", "name" );
+		m.Id = Get(obj, type + "_id", "id" );
+		m.Logo = StringHelper.EscapeUrl(Get(obj, type + "_logo", "logo","album_logo" ));
+	}
+    public static string Get(dynamic obj, params string[] props)
     {
         object res = null;
         if (obj is DynamicJsonObject)
         {
-            res = obj[prop];
-			if(res == null && fallback != null)
-				res = obj[fallback];
+			foreach(var prop in props)
+			{
+				res = obj[prop];
+				if(res != null)
+					break;
+			}
         }
         else
         {
             var dict = obj as IDictionary<string, object>;
-            if (dict.ContainsKey(prop))
-            {
-				res = dict[prop];
-            }
-			else if(fallback!=null && dict.ContainsKey(fallback))
-            {
-				res = dict[fallback];
-            }
+			foreach(var prop in props)
+			{
+				if(dict.ContainsKey(prop))
+				{
+					res = dict[prop];
+					break;
+				}
+				res = obj[prop];
+			}
         }
 
-        return res as string;
+        return string.Format("{0}",res);
     }
 }
